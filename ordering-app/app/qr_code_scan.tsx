5dacd1db5d513c84ccceaeeb99c702f04a79326d
biroferenc075 <biroferenc075@gmail.com>
2024-05-05 14:57:08 +0200

    added qr code scan tab

--- ordering-app/app/qr_code_scan.tsx
+++ ordering-app/app/qr_code_scan.tsx
@@ -5,17 +5,20 @@ import { Text, View } from "@/components/Themed";
 import { router } from 'expo-router';
 import { Button } from "@ui-kitten/components";
 
-export default function CartScreen() {
+
+export default function QrCodeScanScreen() {
+    const onSuccess = (e : any) => {
+        router.navigate("/")
+    };
+    
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Cart</Text>
+      <Text style={styles.title}>QR Code Scan</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <Button onPress={() => router.navigate("/qr_code_scan")}>Scan QR code!</Button>
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
@@ -35,4 +38,21 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  }
 });
