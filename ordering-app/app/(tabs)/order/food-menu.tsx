cda552d3b157632f4a05259ebf56ad9c29dac075
Egon Szabó <szabo.egon2001@gmail.com>
2024-05-06 18:30:51 +0200

    Food menu types

--- ordering-app/app/(tabs)/order/food-menu.tsx
+++ ordering-app/app/(tabs)/order/food-menu.tsx
@@ -5,6 +5,8 @@ import { FoodItem } from "@/models/foodItem";
 import { FoodItemService } from "@/services/foodItemService";
 import CartSnackbar from "@/components/CartSnackbar";
 import { useAppContext } from "@/store/AppContext";
+import { Text } from "@ui-kitten/components";
+import { FoodItemTypes } from "@/models/enums/foodItemTypes";
 
 export default function FoodMenuScreen() {
   const [menuItems, setMenuItems] = useState<FoodItem[]>([]);
@@ -15,12 +17,25 @@ export default function FoodMenuScreen() {
     setMenuItems(items);
   }, []);
 
+  const renderFoodItemsForType = (type: FoodItemTypes) => {
+    return menuItems
+      .filter((item) => item.type == type)
+      .map((item, index) => <FoodCard foodItem={item} key={index} />);
+  };
+
   return (
     <>
       <ScrollView contentContainerStyle={styles.container}>
-        {menuItems.map((item, index) => (
-          <FoodCard foodItem={item} key={index} />
-        ))}
+        <Text style={styles.typeheader}>Appetizers</Text>
+        {renderFoodItemsForType(FoodItemTypes.appetizers)}
+        <Text style={styles.typeheader}>Main Dishes</Text>
+        {renderFoodItemsForType(FoodItemTypes.main)}
+        <Text style={styles.typeheader}>Sides</Text>
+        {renderFoodItemsForType(FoodItemTypes.side)}
+        <Text style={styles.typeheader}>Desserts</Text>
+        {renderFoodItemsForType(FoodItemTypes.dessert)}
+        <Text style={styles.typeheader}>Drinks</Text>
+        {renderFoodItemsForType(FoodItemTypes.drink)}
       </ScrollView>
       {state.cart.length > 0 ? <CartSnackbar /> : <></>}
     </>
@@ -38,5 +53,12 @@ const styles = StyleSheet.create({
     rowGap: 15,
     alignItems: "center",
     backgroundColor: "#f5f5f5",
+  },
+
+  typeheader: {
+    marginVertical: 8,
+    fontSize: 20,
+    fontWeight: "bold",
+    flexBasis: "100%",
   },
 });
