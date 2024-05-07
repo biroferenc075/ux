48292fa03190bbcf11a328644c2609b0f7d82f50
biroferenc075 <biroferenc075@gmail.com>
2024-05-07 19:02:26 +0200

    add borders to allergen selector

--- ordering-app/components/AllergenSelector.tsx
+++ ordering-app/components/AllergenSelector.tsx
@@ -22,10 +22,12 @@ const AllergenSelector: FC<AllergenSelectorProps> = ({}) => {
     return {
       backgroundColor: AllergenHelpers.getColor(allergen),
       borderRadius: 50,
-      borderWidth: 0,
+      borderWidth: 1,
+      boxSizing: "border-box",
       opacity: isAllergenAllowed ? 1 : 0.4,
       width: 45,
       height: 45,
+      borderColor: "white",
     };
   };
 
@@ -34,15 +36,18 @@ const AllergenSelector: FC<AllergenSelectorProps> = ({}) => {
       <View style={styles.itemcontainer}>
         {Object.values(Allergens).map((allergen, index) => (
             <View style={styles.buttoncontainer}  key={index}>
-              <Button
-                size="tiny"
-                style={getButtonStyles(
-                  allergen,
-                  state.allowedAllergens.includes(allergen)
-                )}
-                accessoryLeft={<AllergenBadge allergen={allergen} backgroundVisible = {false}/>}
-                onPress={() => handleButtonPress(allergen)}
-              ></Button>
+                <View style={styles.bg}>
+                    <View style={styles.frame}/>
+                    <Button
+                    size="tiny"
+                    style={getButtonStyles(
+                    allergen,
+                    state.allowedAllergens.includes(allergen)
+                    )}
+                    accessoryLeft={<AllergenBadge allergen={allergen} backgroundVisible = {false}/>}
+                    onPress={() => handleButtonPress(allergen)}
+                ></Button>
+                </View>
               <Text style={styles.text}>
                 {allergen.toString().toUpperCase()[0] +
                   allergen.toString().slice(1)}
@@ -80,7 +85,26 @@ const styles = StyleSheet.create({
 
   text: {
     marginLeft: 10
-  }
+  },
+
+  bg: {
+    backgroundColor: "white",
+    borderRadius: 50,
+  },
+
+  frame: {
+    backgroundColor: "none",
+    borderRadius: 50,
+    borderColor: "white",
+    borderWidth: 1.5,
+    zIndex: 1,
+    width: 40,
+    height: 40,
+    position: "absolute",
+    top: 2.5,
+    left: 2.5,
+    pointerEvents: "box-none"
+  },
 });
 
 export default AllergenSelector;
